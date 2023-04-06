d0a8d7634ebec267cfc0e30ae5e0c06512f64dbc
Hiolia <69538503+Hiolia@users.noreply.github.com>
2023-04-06 21:57:16 +0200

    Update B1.sh

--- B1.sh
+++ B1.sh
@@ -1,15 +1,16 @@
-#!/bin/bash 
+#!/bin/bash
+
+# Your path 
+main_dir=""
+
+# Delete files in main
+find "$main_dir" -maxdepth 1 -type f ! -name "*.mp3" ! -name "*.wav" -delete 
+
+# Delete files in artist
+for artist_dir in "$main_dir"/*; do 
+  if [ ! -d "$artist_dir" ]; then 
+    continue 
+  fi 
   
-# Your path 
-main_dir="" 
-  
-find "$main_dir" -type f ! -name "*.mp3" ! -name "*.wav" -delete 
-find "$main_dir"/* -type f -delete 
-for artist_dir in "$main_dir"/*; do 
-  if [ ! -d "$artist_dir" ]; then 
-    continue 
-  fi 
-  
-  find "$artist_dir"/* -type f ! -name "*.mp3" ! -name "*.wav" -delete 
-  find "$artist_dir"/* -type f -delete 
+  find "$artist_dir" -maxdepth 1 -type f ! -name "*.mp3" ! -name "*.wav" -delete 
 done
