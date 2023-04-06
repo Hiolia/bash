3180ed7a2902e8644b79f2b96902e0f5b4eb24df
Hiolia <69538503+Hiolia@users.noreply.github.com>
2023-04-06 18:10:06 +0200

    Create B3.sh

---
+++ B3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+  
+# Your path
+main_dir="chemin/vers/repertoire/principal" 
+  
+# Remove not .mp3 or .wav files
+for album_dir in $(find "$main_dir"/*/* -type d); do 
+  find "$album_dir" -type f ! -name "*.mp3" ! -name "*.wav" -delete 
+done
