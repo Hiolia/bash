b86bf82d76e09a1fd2542e14d6d88beae2e32e2b
Hiolia <69538503+Hiolia@users.noreply.github.com>
2023-04-07 14:51:09 +0200

    Update B2.sh

--- B2.sh
+++ B2.sh
@@ -1,17 +1,19 @@
-# Your path
-main_dir=""
+#!/bin/bash
 
-# Fix file names 
-for album_dir in $(find "$main_dir"/*/* -type d); do 
-  for track_file in "$album_dir"/*; do 
-    if [ ! -f "$track_file" ]; then 
-      continue 
-    fi 
-  
-    track_name=$(basename "$track_file") 
-    if [[ ! "$track_name" =~ ^[0-9]{2}.* ]]; then 
-      new_track_name=$(printf "%02d - %s" "${track_name:0:1}" "${track_name:2}") 
-      mv "$track_file" "$album_dir/$new_track_name" 
-    fi 
+main_dir=base
+
+for album_dir in $(find "$main_dir"/*/* -type d); do
+  for track_file in "$album_dir"/*; do
+    if [ ! -f "$track_file" ]; then
+      continue
+    fi
+
+    track_name=$(basename "$track_file")
+    if [[ ! "$track_name" =~ ^[0-9]{2}\ .*$ ]]; then
+      new_track_name=$(printf "%02d - %s" "${track_name:0:2}" "${track_name:3}")
+      mv "$track_file" "$album_dir/$new_track_name"
+    fi
   done
 done
+
+
